5a1ef8880d6827cede3710663a047847102e9c95
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2023-11-22 09:24:09 +0200

    feat: add apollo client

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Sovryn Safe Guardian App
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/8b0db8b5-4299-43b8-9390-d233bdc70064/deploy-status)](https://app.netlify.com/sites/safe-guardian/deploys)
+
+
+/// setup apollo client to retrieve bitocracy proposals from the subgraph ///
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { FC, PropsWithChildren } from 'react';
 import { Button } from '@sovryn/ui';
 import { OnboardProvider } from '@sovryn/onboard-react';
+import { ApolloProvider } from '@apollo/client';
 import { useAccount } from './hooks/useAccount';
+import { apollo } from './lib/apollo';
 
 export const App: FC<PropsWithChildren> = ({ children }) => {
   const { address, connect } = useAccount();
   
   return (
-    <>
+    <ApolloProvider client={apollo}>
       {address ? (<>{children}</>) : (<div className='w-full h-screen flex justify-center items-center'><Button text="Connect Wallet" onClick={connect} /></div>)}
       <OnboardProvider />
-    </>
+    </ApolloProvider>
   );
 }
