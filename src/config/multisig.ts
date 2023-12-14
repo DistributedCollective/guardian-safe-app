09a42f0a2a50a4dacee51187055a6eab0d7f3b78
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2023-12-14 09:41:48 +0200

    fix: multisig contract address

--- src/config/multisig.ts
+++ src/config/multisig.ts
@@ -1,6 +1,6 @@
 import { ChainIds } from '@sovryn/ethers-provider';
 
 export const multisigContracts: Partial<Record<ChainIds, string>> = {
-  [ChainIds.RSK_MAINNET]: '0xdd8e07a57560ada0a2d84a96c457a5e6ddd488b7',
+  [ChainIds.RSK_MAINNET]: '0x924f5ad34698fd20c90fe5d5a8a0abd3b42dc711',
   [ChainIds.RSK_TESTNET]: '0x189ecD23E9e34CFC07bFC3b7f5711A23F43F8a57',
 };
